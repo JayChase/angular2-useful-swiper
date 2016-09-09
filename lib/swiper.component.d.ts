53d6068a07983bce92eafa907ad8fed1715bd886
JayChase <JonathanChase@outlook.com>
2016-09-09 17:34:41 +0900

    moved to release

--- lib/swiper.component.d.ts
+++ lib/swiper.component.d.ts
@@ -2,7 +2,6 @@ import { OnInit, ElementRef, AfterViewChecked } from '@angular/core';
 export declare class SwiperComponent implements OnInit, AfterViewChecked {
     private elementRef;
     config: Object;
-    ready: boolean;
     Swiper: any;
     private swiperWrapper;
     private slideCount;
